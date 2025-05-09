6eb38191cb61ed84fe8e0c7bba494bd4917429ef
zqh-wz <zqh23@mails.tsinghua.edu.cn>
2025-05-09 16:47:21 +0800

    fix scripts

--- scripts/run_all_server_A.sh
+++ scripts/run_all_server_A.sh
@@ -2,5 +2,5 @@
 
 for task in fig7 fig8 fig12; do
     echo "Running $task"
-    bash ./scripts/reproduce.sh "$script"
+    bash ./scripts/reproduce.sh "$task"
 done
--- scripts/run_all_server_B.sh
+++ scripts/run_all_server_B.sh
@@ -2,5 +2,5 @@
 
 for task in fig6 fig9 fig10 fig11 fig13 tab3; do
     echo "Running $task"
-    bash ./scripts/reproduce.sh "$script"
+    bash ./scripts/reproduce.sh "$task"
 done
