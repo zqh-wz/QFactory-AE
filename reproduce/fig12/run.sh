3caa36c667f565f068d00f8659d8779184204901
zqh-wz <zqh23@mails.tsinghua.edu.cn>
2025-05-10 01:07:51 +0800

    fix scripts

--- reproduce/fig12/run.sh
+++ reproduce/fig12/run.sh
@@ -7,3 +7,7 @@ export QFACTORY_ARCH=80
 . run_all_vllm.sh
 . run_all_bitblas.sh
 . run_all_llama_cpp.sh
+
+source ../../scripts/activate_env.sh ../../.venv-kernel
+python3 plot_end2end.py
+deactivate
